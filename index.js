6ea65616f73e10eca7951c9ae8004720dc0557e7
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-30 00:45:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let thatBeatle = [];
 function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i < musicians.length; i++) {
     thatBeatle.push(`${musicians} plays ${instruments}`)
-    } else
-  } return thatBeatle;
+    } return thatBeatle;
+  }
