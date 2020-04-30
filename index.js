477f240bbaf693b003bde31c7bb00c834a267c31
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-30 01:09:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+let thatBeatle = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let i = 0; i < musicians.length; i++) {
+    thatBeatle.push(`${musicians[i]} plays ${instruments[i]}`)
+    } return thatBeatle;
+  }
+
+function johnLennonFacts(facts) {
+  let shoutFacts = [];
+
+  let i = 0;
+
+  while (i < facts.length) {
+    shoutFacts.push(`${facts[i]}!!!`)
+    i++
+  } return shoutFacts;
+}
+
+function iLoveTheBeatles(num) {
+  let beaLove = [];
+  do {
+    beaLove.push(`I love the Beatles!`)
+    num++
+  } while (num < 15);
+  return beaLove;
+}
