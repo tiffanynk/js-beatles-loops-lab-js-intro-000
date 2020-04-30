e6f4a788c5ca11ce0859bb4cf521602d98bac58e
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-30 00:44:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+let thatBeatle = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let i = 0; i < musicians.length; i++) {
+    thatBeatle.push(`${musicians} plays ${instruments}`)
+    } else
+  } return thatBeatle;
+}
